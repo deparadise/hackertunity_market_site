54337422ea4cfd4d6ff8e5d2e6d14ee28c98d6c9
David Pineda <david.pineda@deparadise.com>
2021-01-20 13:22:17 -0800

    Add menuLinks metadata configuration to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,54 @@
 module.exports = {
 	siteMetadata: {
 		title: 'Gatsby + Netlify CMS Starter',
-		description:
-			'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+		description: 'Hackertunity client site',
+		menuLinks: [
+			{
+				name: 'About Us',
+				link: '',
+				subMenu: [
+					{
+						name: 'Our Mission',
+						link: '',
+					},
+					{
+						name: 'The Team',
+						link: '',
+					},
+					{
+						name: 'Acknowledgements',
+						link: '',
+					},
+				],
+			},
+			{
+				name: 'Career Catalyst',
+				link: '',
+				subMenu: [
+					{
+						name: 'Training Resources',
+						link: '',
+					},
+					{
+						name: 'Job Feed',
+						link: '',
+					},
+					{
+						name: 'Working Remotely',
+						link: '',
+					},
+					{
+						name: 'Working Remotely',
+						link: '',
+					},
+				],
+			},
+			// {
+			// 	name: 'Covid-19',
+			// 	link: '',
+			// 	subMenu: []
+			// }
+		],
 	},
 	plugins: [
 		'gatsby-plugin-react-helmet',
